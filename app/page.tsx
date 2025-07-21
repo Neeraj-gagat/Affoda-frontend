bc8e4a26f54b78c46813ea13b13f67adf0d33c56
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-07-21 23:13:49 +0530

    fhgs

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,16 @@ import { AppBar } from "@/components/AppBar";
 import { FeraturedProp } from "@/components/Featuredprop";
 import Footer from "@/components/Footer";
 import { Hero } from "@/components/Hero";
-import { Suspense } from "react";
 
 
 
 export default function Home() {
   return (
-    <Suspense fallback={<div className="p-4 text-center">Loading...</div>}>
     <div className="overflow-x-hidden">
       <AppBar/>
       <Hero/>
       <FeraturedProp/>
       <Footer/>
     </div>
-    </Suspense>
   );
 }
