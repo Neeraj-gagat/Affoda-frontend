21da238a7a8e124c7573ec54b5aa3c5da5eb0f5c
Neeraj-gagat <neerajgagat9999@gmail.com>
2025-08-09 22:08:33 +0530

    chngs

--- app/hotels/page.tsx
+++ app/hotels/page.tsx
@@ -1,11 +1,17 @@
 import { Suspense } from "react";
 import HotelsPage from "./HotelResults";
 import HotelCardSkeleton from "@/components/HotelsSkeleton";
+import { AppBar } from "@/components/AppBar";
 
 export default function HotelsPageWrapper() {
   return (
     <Suspense fallback={
-      <HotelCardSkeleton />
+      <div>
+        <AppBar/>
+        {Array.from({length:2}).map((_,idx) => (
+          <HotelCardSkeleton key={idx}/>
+        ))}
+      </div>
     }>
       <HotelsPage />
     </Suspense>
